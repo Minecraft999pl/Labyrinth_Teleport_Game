a099aec28a3812de8a857ec378f790265bf6302a
Minecraft999pl <07kamilw@gmail.com>
2022-10-01 13:15:14 +0200

    Lekcja 3
    -dodano pauzę
    -dodano koniec gry
    -dodano typy podłóż
    -dodano GameManager
    -dodano Stoper

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
     Vector3 velocity;
     CharacterController characterController;
 
+    public Transform groundCheck;
+    public LayerMask groundMask;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,5 +29,26 @@ public class PlayerController : MonoBehaviour
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
         characterController.Move(move * speed * Time.deltaTime);
+
+        RaycastHit hit;
+        if(Physics.Raycast(groundCheck.position, transform.TransformDirection(Vector3.down), out hit, 0.4f, groundMask))
+        {
+            string terrainType;
+            terrainType = hit.collider.gameObject.tag;
+
+            switch(terrainType)
+            {
+                default:
+                    speed = 12;
+                    break;
+                case "Low":
+                    speed = 3;
+                    break;
+                case "High":
+                    speed = 20;
+                    break;
+            }
+        }
     }
+    
 }
