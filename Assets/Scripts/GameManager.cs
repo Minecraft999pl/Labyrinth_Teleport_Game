479a909e9d162951492006e50e076f70c6be1205
Minecraft999pl <07kamilw@gmail.com>
2022-10-08 13:18:46 +0200

    Lekcja 4
    Pickupy

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,17 +9,23 @@ public class GameManager : MonoBehaviour
     bool gamePaused = false;
     bool endGame = false;
     bool win = false;
+    public int points = 0;
+
+    public int redKey = 0;
+    public int greenKey = 0;
+    public int goldKey = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        if(gameManager == null)
+        if (gameManager == null)
         {
             gameManager = this;
         }
 
         InvokeRepeating("Stopper", 2, 1);
 
-        if(timeToEnd <= 0)
+        if (timeToEnd <= 0)
         {
             timeToEnd = 100;
         }
@@ -29,6 +35,7 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         PauseCheck();
+        PickUpCheck();
     }
 
     void Stopper()
@@ -36,12 +43,12 @@ public class GameManager : MonoBehaviour
         timeToEnd --;
         Debug.Log("Time: " + timeToEnd + "s");
 
-        if(timeToEnd <= 0)
+        if (timeToEnd <= 0)
         {
             timeToEnd = 0;
             endGame = true;
         }
-        if(endGame){
+        if (endGame){
             EndGame();
         }
     }
@@ -62,8 +69,8 @@ public class GameManager : MonoBehaviour
 
     void PauseCheck()
     {
-        if(Input.GetKeyDown(KeyCode.P)){
-            if(gamePaused)
+        if (Input.GetKeyDown(KeyCode.P)){
+            if (gamePaused)
             {
                 ResumeGame();
             }
@@ -75,7 +82,7 @@ public class GameManager : MonoBehaviour
     }
     public void EndGame(){
         CancelInvoke("Stopper");
-        if(win)
+        if (win)
         {
             Debug.Log("You win! Reload?");
             //Time.timeScale = 0f;
@@ -84,6 +91,48 @@ public class GameManager : MonoBehaviour
         {
             Debug.Log("You lose! Reload?");
             //Time.timeScale = 0f;
+        }
+    }
+
+    public void AddPoints(int point)
+    {
+        points += point;
+    }
+
+    public void AddTime(int AddTime)
+    {
+        timeToEnd += AddTime;
+    }
+
+    public void FreezeTime(int freeze)
+    {
+        CancelInvoke("Stopper");
+        InvokeRepeating("Stopper", freeze, 1);
+    }
+
+    public void AddKey(KeyColor color)
+    {
+        if (color == KeyColor.Gold)
+        {
+            goldKey++;
+        }
+        else if (color == KeyColor.Green)
+        {
+            greenKey++;
+        }
+        else if (color == KeyColor.Red)
+        {
+            redKey++;
+        }
+    }
+
+    void PickUpCheck()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Debug.Log("Actual Time: " + timeToEnd);
+            Debug.Log("Red keys: " + redKey + " Green keys: " + greenKey + " Gold keys: " + goldKey);
+            Debug.Log("Points: " + points);
         }
     }
 }
